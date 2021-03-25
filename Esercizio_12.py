97bd47448355ab11e0789fc56e8c124ab2371761
Andreamattiello <75073631+Andreamattiello@users.noreply.github.com>
2021-03-25 12:13:04 +0100

    Add files via upload

---
+++ Esercizio_12.py
@@ -0,0 +1,11 @@
+from numpy import mean
+Alunni={
+        "Mario":{"Matematica":6,"Italiano":6,"cienze":7,"Inglese":4},
+        "Giovanni":{"Matematica":4,"Italiano":6,"cienze":5,"Inglese":7},
+        "Paola":{"Matematica":9,"Italiano":6,"cienze":8,"Inglese":8}}
+voti_mario=list(Alunni["Mario"].values())
+voti_giovanni=list(Alunni["Giovanni"].values())
+voti_paola=list(Alunni["Paola"].values())
+print("La media di Mario é: " + str(mean(voti_mario)))
+print("La media di Giovanni é: " + str(mean(voti_giovanni)))
+print("La media di Paola é: " + str(mean(voti_paola)))
